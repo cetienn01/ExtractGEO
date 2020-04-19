6eac2585d99a89f0f0216131fd014edf2401acaa
Chris Etienne <cetienn01@gmail.com>
2020-04-19 17:55:06 -0400

    Update ExtractGEO.py

--- ExtractGEO.py
+++ ExtractGEO.py
@@ -87,20 +87,10 @@ def get_GEO_files(self, series, sample, platform, dataset):
              geo_access_num = [str(geo_access_num) for geo_access_num in input("Enter the GEO accession number here: ").split(",")]
              break
              # geo={}.geo_access_num, destdir=output_dir
-
-            #check that the GEO accession number entered by the uder is valid
-            
-         # If something else that is not a GEO accession number is introduced, the ValueError exception will be called
-         except ValueError:
-             # The cycle will go on until validation
-             print("Error! This GEO accession number is invalid. \n Please enter a valid GEO accession number to continue" )
-             
-         # When a valid GEO accession number is entered successfully, the loop will end.
-         else:
-                 # (1) print all the GEO accession numbers provided by the user (and remove the square brackets from the output of the list)
-                     print("You entered the following GEO accession numbers: " + str(geo_access_num)[1:-1])
-                 # (2) print the total number of inputs provided by the user
-                     print("A total of %d GEO accession number was provided" % len(geo_access_num))
+        # (1) print all the GEO accession numbers provided by the user (and remove the square brackets from the output of the list)
+        print("You entered the following GEO accession numbers: " + str(geo_access_num)[1:-1])
+        # (2) print the total number of inputs provided by the user
+        print("A total of %d GEO accession number was provided" % len(geo_access_num))
                 
         """     
         #another option is to
@@ -115,12 +105,22 @@ def get_GEO_files(self, series, sample, platform, dataset):
             temp_list.append(i)
         print("You entered the following GEO accession numbers: " + str(temp_list)[1:-1])
         print("The total number of GEO accession numbers provided is: ", len(temp_list))
-        """     
+        """
         
-        # if one or more of the GEO accession number is invalid, throw a warning, reject those entries and proceed to download only the valid GEO accession numbers
-        # first, check the list to see if all inputs are valid, if not, find the ones that are invalid and...
-        # (1) remove them from the original list and (2) store into a separate list
-        
+        ## check that the GEO accession number(s) entered by the uder is(are) valid...(3 options - see below)
+        # (1) if all the the inputs are invalid, throw an error and ask the user to enter at least 1 valid input to continue
+        # (2) if one or more of the GEO accession number is invalid, throw a warning, reject those entries and proceed to download only the valid GEO accession numbers
+        ## do this by: [first] remove the invalid inputs from the original list, [second] store the invalid inputs into a separate list,
+        ## [third] download only the valid input from the original list
+        # (3) if all inputs are valid, proceed to download all
+                    
+         # If something else that is not a GEO accession number is introduced, the ValueError exception will be called
+         except ValueError:
+             # The cycle will go on until validation
+             print("Error! This GEO accession number is invalid. \n Please enter a valid GEO accession number to continue" )
+             
+         # When a valid GEO accession number is entered successfully, the loop will end.
+         else:
         print("The following GEO accession numbers are invalid: %s, \n and will be ignored" % )
         print("only the these GEO accession numbers will be downloaded")
         
